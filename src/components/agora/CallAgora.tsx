74f28b81f09eaedd50e3c60ca322eba1a1538950
Brayan Murphy Crespo Espinoza <BrayanMurphyC@gmail.com>
2024-03-07 15:54:12 -0500

    add: añadiendo diseño

--- src/components/agora/CallAgora.tsx
+++ src/components/agora/CallAgora.tsx
@@ -19,11 +19,11 @@ import {
   MdScreenShare,
   MdStopScreenShare,
   MdCallEnd,
-  MdCamera,
   MdOutlineMicNone,
   MdOutlineMicOff,
   MdOutlineVideocam,
   MdOutlineVideocamOff,
+  MdOutlinePersonOff,
 } from "react-icons/md";
 import { Container } from "../shared/Container";
 import { ShareScreenComponent } from "./SharedScreen";
@@ -54,6 +54,17 @@ function Videos(props: { channelName: string; AppID: string }) {
   const [isMuteVideo, setMuteVideo] = useState(false);
   const [isMuteAudio, setMuteAudio] = useState(false);
   const [isSharingEnabled, setScreenSharing] = useState(false); // sirve para compartir pantalla local
+  // seleccionar el video que queremos en la pantalla grande
+  const [selectedUidVideo, setSelectedUidVideo] = useState(0);
+
+  // asignamos el primer usuario que se conecte como el usuario seleccionado
+  useEffect(() => {
+    if (remoteUsers.length > 0) {
+      setSelectedUidVideo(remoteUsers[0].uid as number);
+    } else {
+      setSelectedUidVideo(0);
+    }
+  }, [remoteUsers]);
 
   const handleToggleScreenSharing = () => {
     setScreenSharing((previous) => !previous);
@@ -74,6 +85,13 @@ function Videos(props: { channelName: string; AppID: string }) {
     );
 
   remoteUsers.length > 0 && console.log("remoteUsers", remoteUsers);
+
+  const isActiveRemoteVideo = remoteUsers.find(
+    (user) => user.uid === selectedUidVideo,
+  )?.hasVideo;
+  const isActiveRemoteAudio = remoteUsers.find(
+    (user) => user.uid === selectedUidVideo,
+  )?.hasAudio;
 
   const numUsers = remoteUsers.length + 1;
   let numCols = 1;
@@ -128,49 +146,110 @@ function Videos(props: { channelName: string; AppID: string }) {
 
       <Container className="py-4">
         <div className="relative my-10 flex h-[70vh] w-full flex-col items-center justify-center rounded-lg bg-gradient-to-r from-indigo-500 via-cyan-500 to-gray-500 p-1">
-          {remoteUsers.length > 1 ? (
-            <div className="h-full w-full rounded-lg border-2 shadow-md">
-              <RemoteUser
-                user={remoteUsers[1]}
-                key={remoteUsers[1]?.uid}
-                className="h-full w-full rounded-lg border-2 shadow-md"
-              />
-            </div>
-          ) : (
+          {/* Aca debo de mostrar el video del usuario seleccionado en pantalla grande y reemplazarlo cuando se comparte pantalla */}
+          <div className="h-full w-full rounded-lg border-2 shadow-md">
             <RemoteUser
-              user={remoteUsers[0]}
-              key={remoteUsers[0]?.uid}
-              className="h-full w-full rounded-lg shadow-md"
+              user={
+                remoteUsers.find((user) => user.uid === selectedUidVideo) ||
+                remoteUsers[0]
+              }
+              key={
+                remoteUsers.find((user) => user.uid === selectedUidVideo)
+                  ?.uid || remoteUsers[0]?.uid
+              }
+              className="h-full w-full rounded-lg border-2 shadow-md"
             />
-          )}
+
+            {!isActiveRemoteVideo && (
+              <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center rounded-lg bg-gray-800 bg-opacity-50">
+                <MdOutlineVideocamOff className="text-4xl text-white" />
+              </div>
+            )}
+          </div>
+
           {/* Parte inferior izquierda nombre remoto usuario */}
           <div className="absolute bottom-0 left-0 m-4 flex items-center rounded-full bg-gray-800 bg-opacity-50 p-2 shadow-sm">
+            {isActiveRemoteAudio ? (
+              <MdOutlineMicNone className="mx-1 text-sm text-red-500" />
+            ) : (
+              <MdOutlineMicOff className="mx-1 text-sm text-white" />
+            )}
+
             <span className="relative mr-2 flex h-3 w-3">
               <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
               <span className="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
             </span>
-            <p className="text-[10px] text-white">Remote</p>
+            <p className="text-[10px] text-white">
+              User {selectedUidVideo || 0}
+            </p>
           </div>
 
-          {/* parte inferior derecha el usuario local */}
-          <div className="gradient-border absolute bottom-0  right-0 m-4  h-36 w-36 rounded-xl border-4 shadow-md">
-            <div className={`container h-full w-full rounded-xl`}>
-              <LocalVideoTrack
-                track={localCameraTrack}
-                play={true}
-                className="aspect-w-16 aspect-h-9 aspect-video h-full w-full rounded-xl object-cover shadow-md"
-              />
+          <div
+            className={`absolute bottom-0 right-0 rounded-xl border-4 shadow-md transition-all duration-500 ${remoteUsers.length === 0 ? "flex h-full w-full p-0" : "m-4 h-36 w-36"}`}
+          >
+            {!isMuteVideo ? (
+              <div className={`container h-full w-full rounded-xl`}>
+                <LocalVideoTrack track={localCameraTrack} play={true} />
+              </div>
+            ) : (
+              <div className="container flex h-full w-full items-center justify-center rounded-xl bg-gray-800 bg-opacity-50">
+                <MdOutlinePersonOff className="text-4xl text-white" />
+              </div>
+            )}
 
-              <div className="absolute bottom-0 right-0 flex items-center rounded-full bg-gray-800 bg-opacity-50 p-2">
-                <span className="relative mr-2 flex h-2 w-2">
-                  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
-                  <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
-                </span>
-                <p className="text-[10px] text-white">You</p>
+            <div className="absolute bottom-0 right-0  ">
+              <div className="mb-4 mr-2 flex items-center justify-center rounded-full bg-gray-800 bg-opacity-50 px-2 py-1">
+                <div>
+                  {isMuteAudio ? (
+                    <MdOutlineMicOff className="mx-1 text-sm text-white" />
+                  ) : (
+                    <MdOutlineMicNone className="mx-1 text-sm text-red-500" />
+                  )}
+                </div>
+                <div className="flex flex-row items-center">
+                  <span className="relative mr-2 flex h-2 w-2">
+                    <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+                    <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
+                  </span>
+                  <p className="text-[10px] text-white">You</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+
+        {/* Si tengo mas de 2 usuarios colocarlos en una grilla y que se ordene en un cuadrado con bordes blancos separados y sus nombres de usuario */}
+        {remoteUsers.length > 1 && (
+          <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-gradient-to-r from-indigo-500 via-cyan-500 to-gray-500 py-4 shadow-md">
+            <div className="grid grid-cols-3 gap-4">
+              {/* filtraremos  que  el usuario seleccionado no se muestre en la grilla  */}
+              {remoteUsers
+                .filter((user) => user.uid !== selectedUidVideo)
+                .map((user) => (
+                  <div
+                    key={user.uid}
+                    className="relative h-36 w-36 rounded-xl border-4 shadow-md"
+                    onClick={() => setSelectedUidVideo(user.uid as number)}
+                  >
+                    <RemoteUser
+                      user={user}
+                      key={user.uid}
+                      className="h-full w-full rounded-xl"
+                    />
+                    <div className="absolute bottom-0 right-0 flex items-center rounded-full bg-gray-800 bg-opacity-50 p-2">
+                      <span className="relative mr-2 flex h-2 w-2">
+                        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+                        <span className="relative inline-flex h-2 w-2 rounded-full bg-green-500"></span>
+                      </span>
+                      <p className="text-[10px] text-white">{user.uid}</p>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        )}
+
+        {/* Botones de control */}
 
         <div className="flex justify-between space-x-4 pb-4">
           <Image
