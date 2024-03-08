f05f01bceb4f1bdd4f953927338b700732f9652c
Brayan Murphy Crespo Espinoza <BrayanMurphyC@gmail.com>
2024-03-08 00:30:57 -0500

    ADD: CAMBIANDO VALORES DE VARIBLES DE ENTORNO

--- src/utils/credentials_firebase.ts
+++ src/utils/credentials_firebase.ts
@@ -6,14 +6,13 @@ import "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// TODO: Colocar en .env las credenciales de firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAuh5E9hbVCaWn4dBh2LIBAqxUPBEygluo",
-  authDomain: "clinica-internacional-41a58.firebaseapp.com",
-  projectId: "clinica-internacional-41a58",
-  storageBucket: "clinica-internacional-41a58.appspot.com",
-  messagingSenderId: "731195266634",
-  appId: "1:731195266634:web:821464eccd3f765224b8b3",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
